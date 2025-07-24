0529ea7f2291565d27e0754f60c1fc581342096a
hassanfahmyzaky <hassanfahmyzaky@gmail.com>
2025-07-24 22:05:14 +0300

    dump tryout!

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,6 +1,7 @@
 # Global conversion factors - NOTE THE EXACT SPELLING REQUIRED
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5  # Fixed spelling (was missing 'O' before)
+CELSIUS_TO_FAHRENHEIT_FACTOR\s*=\s*9\/5
 
 def convert_to_celsius(fahrenheit):
     """Convert Fahrenheit to Celsius using the global factor"""
